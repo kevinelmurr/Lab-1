a7c6857069df0d01766fd208c743954593635b96
kevinelmurr <122998298+kevinelmurr@users.noreply.github.com>
2023-01-27 22:43:26 +0200

    Add files via upload

---
+++ Profile.java
@@ -0,0 +1,7 @@
+public class Profile {
+	public static void main(String [] args) {
+	System.out.println("Name is Kevin El Murr");
+	System.out.println(" My Major is Electrical Engineering");
+	System.out.println("First year 2023");
+}
+}
